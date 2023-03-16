259e43af40f1b23ada42ed93cf96e0fe800a490b
jaiden-stipp <business@jstipp.com>
2023-03-15 17:16:49 -0700

    Create script.py

---
+++ script.py
@@ -0,0 +1,10 @@
+import random
+print("Hello! Please enter your name below: ")
+x = str(input())
+print("Welcome " + x + "!")
+class Player():
+    def __init__(self, name, hand, money, bet)
+    
+
+class Dealer():
+    pass
