2e68d9d29df41212abe6249a88f93ca402b4ff04
jaiden-stipp <business@jstipp.com>
2023-03-15 17:43:31 -0700

    Update script.py
    Initialized main inputs for name and money

--- script.py
+++ script.py
@@ -1,10 +1,34 @@
 import random
-print("Hello! Please enter your name below: ")
-x = str(input())
-print("Welcome " + x + "!")
+#Intro text
+player_hand = 0
+dealer_hand = 0
+def play():
+    pass
+print("Hello! Please enter your name below: \n")
+name = str(input())
+print("\nWelcome " + name + "!\n")
+print("How much money are you putting on the table today?\n")
+balance = float(input())
+print("\nCurrent Balance: ${}".format(balance))
+
+def hit():
+    card = randint(1,12)
+    player_hand += card
+    if player_hand > 21:
+        print("\nBust!")
+    elif player_hand == 21:
+
+
 class Player():
-    def __init__(self, name, hand, money, bet)
+    player_name = name
+    player_balance = balance
+    def __init__(self, hand, money, bet):
+        pass
+   
+        
     
 
 class Dealer():
     pass
+
+
