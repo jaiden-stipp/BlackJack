3390aad32ae6b93caebd0e1876657a6889156896
jaiden-stipp <business@jstipp.com>
2023-03-16 11:50:50 -0700

    Update script.py
    Added Hit or Stand Prompt

--- script.py
+++ script.py
@@ -26,10 +26,10 @@ bet = 0
 print("How much would you like to bet on the current game?\n")
 bet_input = input()
 bet += float(bet_input)
-if bet > game_balance:
+if bet > balance:
     print("\nInvalid Bet, please try again")
     quit()
-if bet == game_balance:
+if bet == balance:
     print("Are you sure you'd like to proceed with betting your entire balance? Enter Y for yes and N for no\n")
     exit_or_play = input()
     if exit_or_play == "y" or exit_or_play == 'Y':
@@ -40,7 +40,7 @@ if bet == game_balance:
 class Player():
     player_list = []
 
-    def __init__(self, player_name, player_balance, player_bet, player_hand = 0):
+    def __init__(self, player_name, player_balance, player_bet = 0, player_hand = 0):
         self.name = player_name
         self.balance = player_balance
         self.hand = player_hand
@@ -48,6 +48,9 @@ class Player():
     def draw_card(self):
         card = random.randint(1,11)
         return card
+    def end_game(self):
+        print("\nThanks for playing!")
+        quit()
     def deal_hand(self):
         card1 = Player.draw_card(self)
         card2 = Player.draw_card(self)
@@ -59,35 +62,50 @@ class Player():
         if self.hand == 21:
             self.balance += self.bet
             return "Blackjack!"
+        print("Total:")
         return self.hand
-player1 = Player(name, balance, final_bet)
+    
+    def hit(self):
+        card = Player.draw_card(self)
+        self.hand += card
+        print("\nCard Pulled:", card)
+        print()
+        if self.hand > 21:
+            print("Total:", self.hand)
+            print("\nBust!")
+            quit()
+        elif self.hand < 21:
+            print(self.hand)
+            return
+
+        elif self.hand == 21:
+            print("\nBlackJack!")
+            quit()
+            #win()
+        else:
+            print("You cannot hit")
+    def stand(self):
+        pass
+    
+player1 = Player(name, balance)
+time.sleep(1)
+print("Your Hand:")
 print(player1.deal_hand())
 
+print(player1.hit_stand())
 
 
-def hit():
-    if player_hand < 21:
-        card = draw_card()
-        player_hand += card
-        if player_hand > 21:
-            print("\nBust!")
-        elif player_hand == 21:
-            print("\nBlackJack!")
-            #win()
-    else:
-        print("You cannot hit")
 
 
-class Player():
-    player_name = name
-    player_balance = balance
-    def __init__(self, hand, money, bet):
-        pass
+
+
+
+
    
         
     
 
 class Dealer():
-    pass
-
+    def __init__(self, dealer_hand):
+        self.hand = dealer_hand
 
